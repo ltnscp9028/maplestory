4c4266304f1895869f092601460cb25664d85aa0
y0on2q <ltnscp9028@gmail.com>
2020-05-30 12:25:13 +0900

    Updates

--- precache-manifest.67530e18b1693e86d9dc354b8d664d0c.js
+++ precache-manifest.67530e18b1693e86d9dc354b8d664d0c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "25e9a2ed17fffee6727be28f1ec2f37a",
+    "revision": "50a6f371571ae0d5b147a1bd832e27fe",
     "url": "/maplestory/index.html"
   },
   {
-    "revision": "eba2fa8701a1a19d071e",
-    "url": "/maplestory/static/css/main.0d5ad297.chunk.css"
+    "revision": "976e7f0dd556b9a043bc",
+    "url": "/maplestory/static/css/main.c055b32f.chunk.css"
   },
   {
-    "revision": "ce9e5f281b8c0d5561f3",
-    "url": "/maplestory/static/js/2.c5352f7c.chunk.js"
+    "revision": "900cc2aac6676f7f3392",
+    "url": "/maplestory/static/js/2.425c114b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/maplestory/static/js/2.c5352f7c.chunk.js.LICENSE.txt"
+    "url": "/maplestory/static/js/2.425c114b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eba2fa8701a1a19d071e",
-    "url": "/maplestory/static/js/main.14900b8f.chunk.js"
+    "revision": "976e7f0dd556b9a043bc",
+    "url": "/maplestory/static/js/main.ed7ed221.chunk.js"
   },
   {
     "revision": "9df3fd6064734f5fc23f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/maplestory/precache-manifest.fbb930836e158ec091d4a5a3e26df862.js"
+  "/maplestory/precache-manifest.67530e18b1693e86d9dc354b8d664d0c.js"
 );
 
 self.addEventListener('message', (event) => {
